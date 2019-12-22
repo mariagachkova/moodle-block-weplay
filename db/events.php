952483926adadcb39f98b0ae75fb28c666305966
Mariya Gachkova <mariq.gachkova@gmail.com>
2019-12-22 12:47:54 +0200

    Add access rules for earning points
    Only students can earn points.

--- db/events.php
+++ db/events.php
@@ -3,9 +3,11 @@
 $observers = [
     [
         'eventname'   => '*',
-        'callback'    => 'block_wp\\local\\observer\\observer::catch_all',
-//        'includefile' => null,
+        'callback'    => 'block_weplay\\local\\observer\\observer::catch_all',
         'internal'    => false,
-//        'priority'    => 9999,
+    ],
+    [
+        'eventname' => '\\core\\event\\course_deleted',
+        'callback' => 'block_weplay\\local\\observer\\observer::course_deleted'
     ]
 ];
