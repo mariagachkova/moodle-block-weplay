777bf2adea4efc8675b7e9acf69da7a60fa4b26f
Mariya Gachkova <mariq.gachkova@gmail.com>
2020-01-22 17:56:58 +0200

    Extend leaderboard limit of users

--- leader_board.php
+++ leader_board.php
@@ -25,7 +25,7 @@ $outputL = $PAGE->get_renderer('block_weplay');
 $sql =<<<sql
 SELECT lvl.id, lvl.userid, lvl.courseid, lvl.level, lvl.points, lvl.progress_bar_percent, avt.avatar_title, avt.avatar_name, avt.avatar_description, avt.avatar_image
 FROM {block_wp_level} lvl LEFT OUTER JOIN {block_wp_avatar} avt ON avt.userid = lvl.userid AND avt.courseid = lvl.userid
-WHERE lvl.courseid = $courseid ORDER BY points DESC LIMIT 10
+WHERE lvl.courseid = $courseid ORDER BY points DESC LIMIT 20
 sql;
 
 $combined_records = $DB->get_records_sql($sql);
